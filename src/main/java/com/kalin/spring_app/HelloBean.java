ab6fec58af9ac67fa26a2ff1bd617e3e8f27f896
Vitaly Kalinsky <kalinskyvii@gmail.com>
2023-10-15 15:22:14 +0300

    module 4 HW 7

--- src/main/java/com/kalin/spring_app/HelloBean.java
+++ src/main/java/com/kalin/spring_app/HelloBean.java
@@ -1,8 +1,12 @@
 package com.kalin.spring_app;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class HelloBean {
 	private String message;
-	public HelloBean(String message) {
+	public HelloBean(@Value("hello") String message) {
 		this.message = message;
 	}
 	public String getMessage() {
